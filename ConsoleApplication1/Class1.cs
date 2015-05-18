9ab11b3addf9cb04f8377c9f645f2f8e609766bf
kobezone <kobezone@163.com>
2015-05-18 17:49:54 +0800

    --c# hello world

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Delegate
+{
+    //定义委托，它定义了可以代表的方法的类型
+    public delegate void GreetingDelegate(string name);
+    class Program
+    {
+        private static void EnglishGreeting(string name)
+        {
+            Console.WriteLine("Morning, " + name);
+        }
+        private static void ChineseGreeting(string name)
+        {
+            Console.WriteLine("早上好, " + name);
+        }
+        //注意此方法，它接受一个GreetingDelegate类型的参数，该参数是返回值为空，参数为string类型的方法
+        private static void GreetPeople(string name, GreetingDelegate MakeGreeting)
+        {
+            MakeGreeting(name);
+        }
+        static void Main(string[] args)
+        {
+            GreetPeople("Jimmy Zhang", EnglishGreeting);
+            GreetPeople("张子阳", ChineseGreeting);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C2;
+using System.Linq.Expressions;
+using System.ComponentModel.DataAnnotations;
+
+namespace C
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("hello world! C#");
+        //    //Console.Write(Program1.calc(5500));
+        //    System.AppDomainInitializer ab = x =>
+        //    {
+        //        Console.WriteLine(String.Join(",", x));
+        //    };
+
+        //    ab(new String[] { "abc", "bcd" });
+
+
+        //    IInterface cc = new C.cc();
+        //   var s = cc.hello(new String[]{"c"});
+        //   Console.Write(s);
+        //    Console.WriteLine(cc.GetHashCode()+"");
+        //    C2.red a = (x, y) =>
+        //    {
+        //        return x * 15 * y;
+
+        //    };
+
+        //    Console.Write(a(15, 10));
+
+        //}
+    }
+
+    static class MyClass
+    {
+        public static IList<String> Test(this IList<String> list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class cc : IInterface {
+
+        //[Display(Name = "")]
+        string IInterface.hello(string[] ccc)
+        {
+            String[] cc = { "c", "a", "b" };
+            List<String> cc1 = cc.Reverse().ToList();
+            //  list = new List<String>(cc1);
+            //for (int i = 0; i < cc1.Count; i++)
+            //{
+            //    var count = cc1.OrderBy(x => x).Where(x => x != "a").Skip(1).Take(1);
+            //}
+
+            
+            //Expression<Func<String, Boolean>> t = x => false;
+
+
+            return cc1[0];
+
+
+        }
+
+        int IInterface.x
+        {
+            get { return 100; }
+        }
+
+        private String _cc;
+
+        string IInterface.ccc
+        {
+            get
+            {
+                return _cc;
+            }
+            set
+            {
+                _cc = value;
+            }
+        }
+    }
+
+    interface IInterface
+    {
+        //int x=0;
+        String hello(String[] ccc);
+        int x { get; }
+        String ccc { get; set; }
+    }
+    class Program1
+    {
+
+        private int m1;
+        public int m { get; set; }
+        public static int calc(int m)
+        {
+            //Console.WriteLine(m*m);
+            return m * m;
+        }
+    }
+
+    delegate void red(int xxx);
+}
+
+
+namespace C2
+{
+    delegate int red(int x, int y);
+    //delegate int red(int x, long y);
+}
